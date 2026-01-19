bba76896991297b16468b6e34a845a8fccf4e075
madhurifarakate399-sketch <madhurifarakate399@gmail.com>
2026-01-19 17:56:29 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Arithmatic {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+       
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+        sc.close();
+    }
+}
---
+++ Swapping.java
@@ -0,0 +1,18 @@
+public class Swapping{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;  
+        a = b;         
+        b = temp;      
+
+        System.out.println("\nAfter swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
